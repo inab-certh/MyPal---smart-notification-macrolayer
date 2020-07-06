c66d4b9e294397e660d80508d309cd010ed21ca7
Panos <pbonotis@gmail.com>
2020-07-06 16:40:45 +0300

    Commiting the first changes on how the MacroSchedulHander will work. TODO: complete the methods to be able to request FRAU help.

--- lib/macroScheduleHandlerNEW.dart
+++ lib/macroScheduleHandlerNEW.dart
@@ -1,16 +1,72 @@
+//import 'package:flutter/material.dart';
+
 class MacroScheduleHandler {
-String disease;
-String clinic;
-DateTime preferredTime;
-DateTime appointmentStartingTime;
-bool isAppointmentForTreatment;
+  String disease;
+  String clinic;
+  DateTime preferredTime;
+  DateTime appointmentStartingTime;
+  bool isAppointmentForTreatment;
 
-MacroScheduleHandler({this.disease, this.clinic, this.preferredTime, this.appointmentStartingTime, this.isAppointmentForTreatment});
+  MacroScheduleHandler(
+      {this.disease,
+      this.clinic,
+      this.preferredTime,
+      this.appointmentStartingTime,
+      this.isAppointmentForTreatment});
 
-  int calculatePrioritiesBasedOnClinic() {
-    switch (disease) {
-      case 'KI':
+  List<int> getPriorityForAppointmentWithNoTreatment() {
+    List<int> priorityOfAppointment = new List();
+    if (disease == 'CLL') {
+      switch (clinic) {
+        case 'KI':
+          priorityOfAppointment.add(2);
+          break;
 
+        case 'FN BRNO':
+          priorityOfAppointment.add(1);
+          break;
+
+        case 'USR':
+          priorityOfAppointment.add(2);
+          priorityOfAppointment.add(1);
+          break;
+      }
+    } else if (disease == 'MDS') {
+      priorityOfAppointment.add(2);
+      priorityOfAppointment.add(1);
+    } else {
+      //TODO: check print
+      print(
+          'error in getPriority method of MacroScheduleHandler class at the disease if statement');
     }
+    return priorityOfAppointment;
   }
+
+  List<int> getPriorityForAppointmentWithTreatment() {
+    List<int> priorityOfAppointment = new List();
+    if (disease == 'CLL') {
+      switch (clinic) {
+        case 'KI':
+          priorityOfAppointment.add(2);
+          priorityOfAppointment.add(1);
+          break;
+
+        case 'FN BRNO':
+          priorityOfAppointment.add(2);
+          break;
+
+        case 'USR':
+          priorityOfAppointment.add(2);
+          priorityOfAppointment.add(1);
+          break;
+      }
+    } else {
+      //TODO: check print
+      print(
+          'error in getPriority method of MacroScheduleHandler class at the disease if statement');
+    }
+    return priorityOfAppointment;
+  }
+
+  List<int> CalculateStartingHourOfCllPatientAppointmentWithNoTreatment() {}
 }
